59a2eaa6fef0dfd78dea0fc733551ce57c209c76
Terry Sigle <terrysigle@pingidentity.com>
2022-10-03 13:01:53 -0500

    v0.0.1

---
+++ main.tf
@@ -0,0 +1,85 @@
+
+
+
+resource "pingone_environment" "env_instance" {
+  name        = var.target_environment_name
+  description = var.target_environment_description
+  type        = "SANDBOX"
+  license_id  = var.license_id
+
+  default_population {
+    name        = "My Population"
+    description = "My new population for users"
+  }
+
+  dynamic "service" {
+    for_each = var.create_authorize ? [1] : []
+
+    content {
+      type = "Authorize"
+    }
+  }
+
+  dynamic "service" {
+    for_each = var.create_davinci ? [1] : []
+
+    content {
+      type = "DaVinci"
+    }
+  }
+
+  dynamic "service" {
+    for_each = var.create_fraud ? [1] : []
+
+    content {
+      type = "Fraud"
+    }
+  }
+
+  dynamic "service" {
+    for_each = var.create_mfa ? [1] : []
+
+    content {
+      type = "MFA"
+    }
+  }
+
+  dynamic "service" {
+    for_each = var.create_risk ? [1] : []
+
+    content {
+      type = "Risk"
+    }
+  }
+
+  dynamic "service" {
+    for_each = var.create_verify ? [1] : []
+
+    content {
+      type = "Verify"
+    }
+  }
+
+  service {
+    type = "SSO"
+  }
+
+}
+
+data "pingone_role" "identity_data_admin" {
+  name = "Identity Data Admin"
+}
+
+resource "pingone_role_assignment_user" "admin_role" {
+  # count                = var.admin_user_id != "" && var.admin_environment_id != "" ? 1 : 0
+  environment_id       = var.admin_environment_id
+  user_id              = var.admin_user_id
+  role_id              = data.pingone_role.identity_data_admin.id
+  scope_environment_id = pingone_environment.env_instance.id
+}
+
+
+
+
+
+
---
+++ variables.tf
@@ -0,0 +1,66 @@
+
+variable "license_id" {
+  description = "PingOne License ID"
+  type        = string
+}
+
+variable "admin_environment_id" {
+  description = "PingOne Admin Environment ID"
+  type        = string
+  default     = ""
+}
+
+
+variable "target_environment_name" {
+  description = "PingOne Target Environment Name"
+  type        = string
+}
+
+
+variable "target_environment_description" {
+  description = "PingOne Target Environment Description"
+  type        = string
+  default     = ""
+}
+
+variable "admin_user_id" {
+  description = "Target Environment Admin User ID"
+  type        = string
+  default     = ""
+}
+
+variable "create_authorize" {
+  description = "Create the PingOne Authorize Service"
+  type        = bool
+  default     = false
+}
+
+variable "create_davinci" {
+  description = "Create the PingOne DaVinci Service"
+  type        = bool
+  default     = false
+}
+
+variable "create_fraud" {
+  description = "Create the PingOne Fraud Service"
+  type        = bool
+  default     = false
+}
+
+variable "create_mfa" {
+  description = "Create the PingOne MFA Service"
+  type        = bool
+  default     = false
+}
+
+variable "create_risk" {
+  description = "Create the PingOne Risk Service"
+  type        = bool
+  default     = false
+}
+
+variable "create_verify" {
+  description = "Create the PingOne Verify Service"
+  type        = bool
+  default     = false
+}
