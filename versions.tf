a6f8201aefdf1b866ad9f88fd84a18c79b207109
Terry Sigle <terrysigle@pingidentity.com>
2022-10-03 13:37:24 -0500

    fix versions

--- versions.tf
+++ versions.tf
@@ -4,13 +4,5 @@ terraform {
       source = "pingidentity/pingone"
       #version = "~> 0.1"
     }
-    davinci = {
-      source = "samir-gandhi/davinci"
-      #version = "~> 0.1"
-    }
-    
-    http = {
-      source = "hashicorp/http"
-    }
   }
 }
