dd608f23e29db7b423ae4005ec5d8dc9993ac5f2
Terry Sigle <terrysigle@pingidentity.com>
2022-10-03 16:38:50 -0500

    Update to pingone-davinci submodule

--- modules/pingone-davinci/main.tf
+++ modules/pingone-davinci/main.tf
@@ -2,20 +2,27 @@
 
 
 data "http" "flow_json" {
+  count = var.flow_url != null ? 1 : 0
+
   url = var.flow_url
   request_headers = {
     Accept = "application/json"
   }
 }
 resource "davinci_flow" "flow_json" {
-  flow_json = data.http.flow_json.response_body
+  count = var.flow_url != null ? 1 : 0
+
+  flow_json = data.http.flow_json[0].response_body
 
   deploy = true
-
-  # depends_on = [
-  #   pingone_role_assignment_user.admin_role
-  # ]
 }
+resource "davinci_connection" "connection" {
+  count = var.connection.id != null ? 1 : 0
+
+  name         = var.connection.name
+  connector_id = var.connection.id
+}
+
 
 
 
--- modules/pingone-davinci/variables.tf
+++ modules/pingone-davinci/variables.tf
@@ -2,4 +2,17 @@
 variable "flow_url" {
   description = "DaVinci Flow URL containing JSON"
   type        = string
+  default     = null
+}
+
+variable "connection" {
+  description = "DaVinci Connection Info"
+  type = object({
+    name = string
+    id   = string
+  })
+  default = {
+    name = null
+    id   = null
+  }
 }
