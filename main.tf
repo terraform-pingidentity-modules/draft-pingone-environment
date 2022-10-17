2b4039c3ff82b0ad5c2fd8f33be38c6274bedabe
Patrick Cowland <patrickcowland@pingidentity.com>
2022-10-17 12:26:59 +0100

    updates

--- main.tf
+++ main.tf
@@ -1,17 +1,28 @@
-
+#########################################################################
+# PineOne Admin Environment
+#########################################################################
+data "pingone_environment" "administrators" {
+  name = try(var.admin_environment_name, "Administrators")
+}
 
 #########################################################################
 # PingOne License ID
 #########################################################################
 data "pingone_licenses" "org_licenses" {
-  organization_id = var.organization_id
+  count = var.license_name != null && var.license_name != "" ? 1 : 0
+
+  organization_id = var.organization_id != null && var.organization_id != "" ? var.organization_id : data.pingone_environment.administrators.organization_id
 
   data_filter {
     name   = "name"
     values = [var.license_name]
   }
-}
 
+  data_filter {
+    name   = "status"
+    values = ["ACTIVE"]
+  }
+}
 
 #########################################################################
 # PineOne Environment
@@ -20,7 +31,14 @@ resource "pingone_environment" "env_instance" {
   name        = var.target_environment_name
   description = var.target_environment_description
   type        = "SANDBOX"
-  license_id  = data.pingone_licenses.org_licenses.ids[0]
+  license_id  = var.license_id != null && var.license_id != "" ? var.license_id : data.pingone_licenses.org_licenses[0].ids[0]
+
+  lifecycle {
+    precondition {
+      condition     = (var.license_id != null && var.license_id != "") || ((var.license_name != null && var.license_name != "") && length(data.pingone_licenses.org_licenses) == 1)
+      error_message = "Ensure one of `license_id` or `license_name` is set.  If using `license_name`, only one license must be returned."
+    }
+  }
 
   default_population {
     name        = "My Population"
@@ -97,17 +115,19 @@ data "pingone_role" "environment_admin" {
 # PingOne Role Assignment
 #########################################################################
 resource "pingone_role_assignment_user" "identity_data_admin_role" {
-  # count                = var.admin_user_id != "" && var.admin_environment_id != "" ? 1 : 0
-  environment_id       = var.admin_environment_id
-  user_id              = var.admin_user_id
+  count = var.admin_user_assign_identity_admin_role && length(var.admin_user_id_list) > 0 ? length(var.admin_user_id_list) : 0
+
+  environment_id       = var.admin_environment_id != null && var.admin_environment_id != "" ? var.admin_environment_id : data.pingone_environment.administrators.id
+  user_id              = var.admin_user_id_list[count.index]
   role_id              = data.pingone_role.identity_data_admin.id
   scope_environment_id = pingone_environment.env_instance.id
 }
 
 resource "pingone_role_assignment_user" "environment_admin_role" {
-  # count                = var.admin_user_id != "" && var.admin_environment_id != "" ? 1 : 0
-  environment_id       = var.admin_environment_id
-  user_id              = var.admin_user_id
+  count = var.admin_user_assign_environment_admin_role && length(var.admin_user_id_list) > 0 ? length(var.admin_user_id_list) : 0
+
+  environment_id       = var.admin_environment_id != null && var.admin_environment_id != "" ? var.admin_environment_id : data.pingone_environment.administrators.id
+  user_id              = var.admin_user_id_list[count.index]
   role_id              = data.pingone_role.environment_admin.id
   scope_environment_id = pingone_environment.env_instance.id
 }
--- variables.tf
+++ variables.tf
@@ -1,24 +1,50 @@
 
 variable "organization_id" {
-  description = "PingOne Organization ID - Used to obtain license_id"
+  description = "PingOne Organization ID - Used to obtain license_id.  If set, will take precidence over the organization ID found from using the `admin_environment_name` parameter."
   type        = string
+  default     = null
+}
+
+variable "license_id" {
+  description = "PingOne License ID to assign to the environment.  If set, will take precidence over the license ID found from using the `license_name` parameter."
+  type        = string
+  default = null
 }
 
 variable "license_name" {
   description = "PingOne License Name - Used to obtain license_id"
   type        = string
+  default = null
+}
+
+variable "admin_environment_name" {
+  description = "PingOne Admin Environment Name, used to assign admin user role permissions and lookup the current organization ID.  Defaults to `Administrators`."
+  type        = string
+  default     = "Administrators"
 }
 
 variable "admin_environment_id" {
-  description = "PingOne Admin Environment ID"
+  description = "PingOne Admin Environment ID, used to assign admin user role permissions and lookup the current organization ID.  If set, will take precidence over the `admin_environment_name` parameter."
   type        = string
-  default     = ""
+  default     = null
 }
 
-variable "admin_user_id" {
+variable "admin_user_id_list" {
   description = "PingOne Environment Admin User ID"
-  type        = string
-  default     = ""
+  type        = list(string)
+  default     = []
+}
+
+variable "admin_user_assign_identity_admin_role" {
+  description = "Assign the `Identity Data Admin` role to users set in the `admin_user_id_list` parameter."
+  type        = bool
+  default     = true
+}
+
+variable "admin_user_assign_environment_admin_role" {
+  description = "Assign the `Environment Admin` role to users set in the `admin_user_id_list` parameter."
+  type        = bool
+  default     = true
 }
 
 
@@ -31,7 +57,7 @@ variable "target_environment_name" {
 variable "target_environment_description" {
   description = "PingOne Target Environment Description"
   type        = string
-  default     = ""
+  default     = null
 }
 
 variable "create_authorize" {
